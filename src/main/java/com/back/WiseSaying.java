6e1739a805d0c1ad9d4dff4ca700c483003d5b3f
hannahyang116 <hannayang116@gmail.com>
2026-04-28 22:54:05 +0900

    배열 버전

---
+++ src/main/java/com/back/WiseSaying.java
@@ -0,0 +1,32 @@
+package com.back;
+
+import java.util.Formatter;
+
+public class WiseSaying {
+    private int id;
+    private String content;
+    private String author;
+    public WiseSaying(int id, String content, String author){
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+    public int getId(){
+        return this.id;
+    }
+    public String getContent() {
+        return this.content;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
