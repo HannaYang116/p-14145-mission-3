ca64ec7cbf1ac5d9530b9f2f7b8207deb0588818
hannahyang116 <hannayang116@gmail.com>
2026-05-06 10:29:02 +0900

    [BE12]양한나

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,14 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.27.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
+
+    testCompileOnly("org.projectlombok:lombok:1.18.38")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.38")
 }
 
 tasks.test {
